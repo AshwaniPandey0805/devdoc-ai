5cd5952606b242dd55aa8ef649fca3110df16f6e
ashwani <ashwanipandey679@gmail.com>
2026-09-25 14:46:48 +0530

    Removed folder and file for secuity issue.

--- backend/dist/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { createUser, listUsers } from "../controllers/userController.js";
-const router = express.Router();
-router.post("/", createUser);
-router.get("/", listUsers);
-export default router;
